4ae314f0d9616b0eef3f02c4d8181559a75845e5
Denisse Solorzano <dsolorzano3@ucol.mx>
2023-11-17 22:54:52 -0600

    Primera version

---
+++ app.js
@@ -0,0 +1,55 @@
+//Funciones
+
+/*function suma(a,b,...c){
+    return console.log(a,b,c);
+}
+const numero = [5,6,7];
+
+let resultadodefuncion = suma(3,4,...numero)
+console.log(resultadodefuncion);
+*/
+
+/*
+function funcionSaludar(nombre){
+    console.log("Hola"+nombre+"desde una arrow")
+}
+funcionSaludar("Denisse");
+
+let hola = function(nombre){
+    console.log("Hola"+nombre+"Mundo")
+};
+
+hola("gael");
+
+//arrow function, funciones flecha, lambda, fat arrow
+
+numero+=numero
+
+let holaArrow = (nombre) => console.log("Hola"+nombre+ "desde una arrow")
+
+holaArrow("deni");
+
+//callback=una funcion que se ejecuta dentro de otra funcion
+*/
+
+
+/*
+function suma(a,b){
+    console.log("la suma es"+(a+b));}
+
+suma(5,6);
+*/
+
+
+/*
+function suma(x,y) {
+    let resultado= x+y;
+    console.log(resultado)
+}
+function resta(x,y) {
+    let resultado= x-y;
+    console.log(resultado)
+}
+ suma(5,6);
+ console.log("El resultado de la suma es: "+resultado);
+*/
---
+++ index.js
@@ -0,0 +1,76 @@
+//Estructuras de control
+//Condicionales y ciclos
+//while, do while, for
+
+             // indice ; condicionales; incremento o decremento; 
+
+/*            
+for (let index=1; index < 6; index++) {
+    if(index==3) {
+      continue;  
+}
+    debugger;
+}
+*/
+
+//Funciones
+//function saludar(){console.log("Hola mundo");}
+
+function limpiar() {
+    document.getElementById('formulario').reset();
+    
+}
+
+function suma() {
+    let x = parseInt(document.getElementById('numero1').value);
+    let y = parseInt(document.getElementById('numero2').value);
+    document.getElementById('resultado').innerHTML = x + y ;
+}
+function resta() {
+    let x = parseInt(document.getElementById('numero1').value);
+    let y = parseInt(document.getElementById('numero2').value);
+    document.getElementById('resultado').innerHTML = x - y ;
+}
+function multiplicacion() {
+    let x = parseInt(document.getElementById('numero1').value);
+    let y = parseInt(document.getElementById('numero2').value);
+    document.getElementById('resultado').innerHTML = x * y ;
+}
+function division() {
+    let x = parseInt(document.getElementById('numero1').value);
+    let y = parseInt(document.getElementById('numero2').value);
+    document.getElementById('resultado').innerHTML = x / y ;
+}
+
+
+//Funcion que maneja el evento click
+//?=operador ternario
+let boton = document.querySelector(".boton");
+//eventos del mouse
+                                   //Eventio que va a escuchar, funcion que se va a ejecutar
+
+boton.addEventListener("click", function(){
+    console.log("Diste clic en el boton");
+    boton.classList.toggle("verde");
+})
+
+boton.addEventListener("mouseout", function(){
+    document.write("Estamos fuera del boton");
+    boton.classList.toggle("verde");
+})
+
+window.addEventListener("keydown", (e) =>{
+    document.write("pulso una tecla");
+    document.write(String.fromCharCode(e.keyCode))
+} )
+
+
+window.addEventListener("keypress", (e) =>{
+    document.write("pulso una tecla");
+    document.write(String.fromCharCode(e.keyCode))
+} )
+
+window.addEventListener("keyup", (e) =>{
+    document.write("pulso una tecla");
+    document.write(String.fromCharCode(e.keyCode))
+} )  
