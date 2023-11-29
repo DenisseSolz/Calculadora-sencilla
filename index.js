bba4f9537e4edebe9c46b0dc0c0d1da2ffb10c20
Denisse Solorzano <dsolorzano3@ucol.mx>
2023-11-29 16:21:40 -0600

    Segunda version

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ for (let index=1; index < 6; index++) {
 //Funciones
 //function saludar(){console.log("Hola mundo");}
 
+/*
 function limpiar() {
     document.getElementById('formulario').reset();
     
@@ -41,20 +42,20 @@ function division() {
     let y = parseInt(document.getElementById('numero2').value);
     document.getElementById('resultado').innerHTML = x / y ;
 }
-
+*/
 
 //Funcion que maneja el evento click
 //?=operador ternario
-let boton = document.querySelector(".boton");
+//let boton = document.querySelector(".boton");
 //eventos del mouse
                                    //Eventio que va a escuchar, funcion que se va a ejecutar
-
+/*
 boton.addEventListener("click", function(){
     console.log("Diste clic en el boton");
     boton.classList.toggle("verde");
 })
-
-boton.addEventListener("mouseout", function(){
+*/
+/*boton.addEventListener("mouseout", function(){
     document.write("Estamos fuera del boton");
     boton.classList.toggle("verde");
 })
@@ -73,4 +74,45 @@ window.addEventListener("keypress", (e) =>{
 window.addEventListener("keyup", (e) =>{
     document.write("pulso una tecla");
     document.write(String.fromCharCode(e.keyCode))
-} )  
+} ) 
+*/
+let pantalla = document.querySelector(".pantalla");
+const btn = document.querySelectorAll(".botones");
+
+btn.forEach(boton => {
+    boton.addEventListener("click" , () => {
+
+       const BotonApretado = boton.textContent; 
+
+       if (boton.id === "AC"){
+          pantalla.textContent = "0"
+          return;
+         }
+     
+       if (boton.id === "C"){
+          if(pantalla.textContent.length === 1 || pantalla.textContent === "Sintax Error"){
+               pantalla.textContent = "0";
+          }else{
+               pantalla.textContent = pantalla.textContent.slice (0, -1);
+          }
+          return;
+       }
+
+       if (boton.id === "igual"){
+          try {
+               pantalla.textContent = eval(pantalla.textContent);//Eval sirve para evaluar un conjunto den string que tengan caracteristicas matematicas
+          } catch {
+               pantalla.textContent = "Sintax Error"
+          }
+         
+          return;
+     }
+       if (pantalla.textContent === "0" || pantalla.textContent === "Sintax Error") {
+            pantalla.textContent = BotonApretado;
+       }else{
+            pantalla.textContent += BotonApretado;
+       }
+       
+      
+    })
+}) 
